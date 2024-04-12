ed61d5b1471b6629afb7ffebc6b21179aa3873ec
Aleksandr Kalinin <denver55all@hotmail.com>
2024-04-12 17:05:58 +0300

    Added logic description about project

--- src/data/mocks/disclaimer.js
+++ src/data/mocks/disclaimer.js
@@ -1,6 +1,7 @@
-const mockDisclaimerData = `This is a website of companies offering financial services - SFoxInsurance, SFoxInsurance Liising.
-                  
-Before entering into any agreement read and conditions of respective service. Consult a specialist, where necessary.
+const mockDisclaimerData = `This website is a SFoxInsurance engine Demo version.
+This website uses cookies and local storage to save your data, you can at any time reset 
+this data by using Settings button (click two times on start application) and Reset Settings.
+Username: player, pwd: ads123 to show Account Details.
 SFoxInsurance does not provide a credit advisory service for the purposes of the Insurance Intermediates Act.
 The borrower makes the decision of taking out a loan, who accesses the suitability of the loan product and contractual terms to his/her personal
 loan interest, need and financial situation on the basis of the information and warnings presented by the bank
