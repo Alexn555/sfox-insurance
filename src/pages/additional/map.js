48a08e107e0fb49097794a9db9143b2f53ac5982
Aleksandr Kalinin <denver55all@hotmail.com>
2024-04-23 07:43:46 +0300

    Added Map paddings imrpvoements

--- src/pages/additional/map.js
+++ src/pages/additional/map.js
@@ -13,11 +13,11 @@ class MapForm extends HTMLElement {
         <div id="map" class="map">
           <h2>${title}</h2>
           <iframe
-              id="mapOpenStreet"
-              title="Map"
-              width="${w}"
-              height="${h}"
-              src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik">
+            id="mapOpenStreet"
+            title="Map"
+            width="${w}"
+            height="${h}"
+            src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik">
           </iframe>
         </div>
       `;
@@ -26,19 +26,19 @@ class MapForm extends HTMLElement {
     render() {
       this.shadow.innerHTML = `
           <style>
-              .map-wrapper {
-                padding: 20px 0 20px 0;
-                .map {
-                  width: fit-content;
-                  padding: 0 10px 0 10px;
-                  border: 1px dotted grey;
-                }
+            .map-wrapper {
+              padding: 20px 0 20px 0;
+              .map {
+                width: fit-content;
+                padding: 0 10px 0 10px;
+                border: 1px dotted grey;
+              }
             }
           </style>
           <form>
-              <div class="map-wrapper">
-                ${this.showContent(600, 400)}
-              </div>
+            <div class="map-wrapper">
+              ${this.showContent(600, 400)}
+            </div>
           </form>
        `;
     }
