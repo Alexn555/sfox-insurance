415d72007019372d40ddf2eb32bb16b1762a3353
Aleksandr Kalinin <denver55all@hotmail.com>
2024-04-23 07:41:54 +0300

    Added Map improvements

--- src/pages/additional/map.js
+++ src/pages/additional/map.js
@@ -1,6 +1,3 @@
-// @ts-nocheck
-import { ButtonTypes } from '../../components/common/ui';
-
 class MapForm extends HTMLElement {
     constructor() {
       super();
@@ -11,15 +8,15 @@ class MapForm extends HTMLElement {
       this.render();
     }
 
-    showContent() {
+    showContent(w = 600, h = 400, title = 'Map board') {
       return `
         <div id="map" class="map">
-          <h2>Map board</h2>
+          <h2>${title}</h2>
           <iframe
               id="mapOpenStreet"
               title="Map"
-              width="600"
-              height="400"
+              width="${w}"
+              height="${h}"
               src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik">
           </iframe>
         </div>
@@ -28,21 +25,21 @@ class MapForm extends HTMLElement {
   
     render() {
       this.shadow.innerHTML = `
-            <style>
-               .map-wrapper {
-                  padding: 20px 0 20px 0;
-                  .map {
-                    width: fit-content;
-                    padding: 0 10px 0 10px;
-                    border: 1px dotted grey;
-                  }
-              }
-            </style>
-            <form>
-                <div class="map-wrapper">
-                  ${this.showContent()}
-                </div>
-           </form>
+          <style>
+              .map-wrapper {
+                padding: 20px 0 20px 0;
+                .map {
+                  width: fit-content;
+                  padding: 0 10px 0 10px;
+                  border: 1px dotted grey;
+                }
+            }
+          </style>
+          <form>
+              <div class="map-wrapper">
+                ${this.showContent(600, 400)}
+              </div>
+          </form>
        `;
     }
   }
@@ -50,4 +47,3 @@ class MapForm extends HTMLElement {
   if ("customElements" in window) {
     customElements.define("map-form", MapForm);
   }
-  
