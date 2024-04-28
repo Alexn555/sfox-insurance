fd743a25e30cde220265ab270a062969529c3ce7
Aleksandr Kalinin <denver55all@hotmail.com>
2024-04-28 08:44:16 +0300

    Added LoggerService improvements

--- src/services/loggerService.js
+++ src/services/loggerService.js
@@ -1,18 +1,18 @@
 export default class LoggerService {
     static log() {
-        for(let i=0; i < arguments.length; i++ ) {
+        for(let i=0, c = arguments.length; i < c; i++ ) {
             console.log(arguments[i]);
         }
     }
 
     static warn() {
-        for(let i=0; i < arguments.length; i++ ) {
+        for(let i=0, c = arguments.length; i < c; i++ ) {
             console.warn(arguments[i]);
         }
     }
 
     static error() {
-        for(let i=0; i < arguments.length; i++ ) {
+        for(let i=0, c = arguments.length; i < c; i++ ) {
             console.error(arguments[i]);
         }
     }
