5053bf02e251af32d322eaf2d5e91b3894504400
Aleksandr Kalinin <denver55all@hotmail.com>
2024-05-04 18:11:41 +0300

    Added better position to isMobile

--- src/services/platform/mobile.js
+++ src/services/platform/mobile.js
@@ -1,13 +1,13 @@
 import { GlobalSizes } from '../../settings';
 
 export class MobileService {
+    static isMobile() {
+        return window.innerWidth < GlobalSizes.mobileMax;
+    }
+
     static isPlatform() {
         return /iPhone|iPad|iPod|Android|Blackberry/i.test(navigator.userAgent);
     }
-
-    static isMobile() {
-        return window.innerWidth < GlobalSizes.mobileMax;
-     }
 
     static isTouchDevice(method = 1) {
         let isTouch = navigator.maxTouchPoints || 'ontouchstart' in document.documentElement;
