cb1c624c89e549444e71ecb4a02bb3179577f1ba
Aleksandr Kalinin <denver55all@hotmail.com>
2024-06-01 18:05:44 +0300

    Added ScreenQuery improvements

--- src/styles/query.js
+++ src/styles/query.js
@@ -1,11 +1,17 @@
+const sizes = {
+    xs: 768,
+    ds: 768,
+    md: 1220
+};
+
 export default class ScreenQuery {
-    static mobile(ct, w = 768) {
+    static mobile(ct, w = sizes.xs) {
         return `@media (max-width: ${w}px) { ${ct} } `;
     }
-    static desk(ct, w = 768) {
+    static desk(ct, w = sizes.ds) {
         return `@media (min-width: ${w}px) { ${ct} } `;
     }
-    static medium(ct, w = 1220) {
+    static medium(ct, w = sizes.md) {
         return `@media (max-width: ${w}px) { ${ct} } `;
     }
     static combo(ctMob, ctDesk, ctMedium) {
