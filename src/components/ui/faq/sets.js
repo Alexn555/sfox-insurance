9df588d607f4a1393b00a0da301f61148d7d7bc8
Aleksandr Kalinin <denver55all@hotmail.com>
2024-06-17 19:14:04 +0300

    Added FAQ settings improvements

--- src/components/ui/faq/sets.js
+++ src/components/ui/faq/sets.js
@@ -1,5 +1,17 @@
 import { Cursors } from '../../../enums';
 
+const commonSets = {
+    pads: {
+        item: '8px 4px',
+        name: '4px',
+        content: '4px'
+    },
+    fonts: {
+        wrapper: 'smaller',
+        name: '16px'
+    }
+};
+
 export const FAQSets = {
     propertyAmount: 3,
     common: {
@@ -7,29 +19,15 @@ export const FAQSets = {
         enabled: true,
         collapse: true,
         nameCursor: Cursors.normal, // Cursors.pointer
-        pads: {
-            item: '8px 4px',
-            name: '4px',
-            content: '4px'
-        },
-        fonts: {
-            wrapper: 'smaller',
-            name: '16px'
-        }
+        pads: commonSets.pads,
+        fonts: commonSets.fonts
     },
     faqPage: {
         id: 'faqPage',
         enabled: true,
         collapse: true,
         nameCursor: Cursors.normal, // Cursors.pointer
-        pads: {
-            item: '8px 4px',
-            name: '4px',
-            content: '4px'
-        },
-        fonts: {
-            wrapper: 'smaller',
-            name: '16px'
-        }
+        pads: commonSets.pads,
+        fonts: commonSets.fonts
     }
 };
